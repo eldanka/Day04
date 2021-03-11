7c871eedbd3df24c1db61f2808cd12150a517466
eldanka <eldankahrovic2208@gmail.com>
2021-03-11 19:54:49 +0100

    Adding ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    myFirstName = "Eldan";
+    myLastName = "Kahrovic";
+    myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+modul.exports = main;
